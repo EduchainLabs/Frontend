cffa13cb40f74a66e1ef9ec4b1b244b67aa67939
AdityaAnandCodes <adityaanandatwork276@gmail.com>
2025-04-05 00:36:49 +0530

    feat: add loading state and unauthorized access handling in challenges page

--- app/challenges/page.tsx
+++ app/challenges/page.tsx
@@ -2,6 +2,7 @@
 import CompetitionsSection from "@/components/CompetetionsSection"
 import LoginButton from "@/components/LoginButton"
 import Navbar from "@/components/Navbar"
+import UnauthorizedAccess from "@/components/UnauthorizedAccess"
 import {useOCAuth } from "@opencampus/ocid-connect-js"
 
 
@@ -9,9 +10,17 @@ const page = () => {
     const {isInitialized, authState,ocAuth} = useOCAuth()
   return (
     <section className="relative">
-      
+      {!isInitialized && (
+        <div className="absolute inset-0 flex items-center justify-center bg-white/50 dark:bg-black/50 z-10">
+          Loading...
+        </div>
+      )}
       <Navbar />
-      <CompetitionsSection />
+      {isInitialized && !authState.isAuthenticated ? (
+        <UnauthorizedAccess />
+      ) : (
+        <CompetitionsSection />
+      )}
     </section>
   );}
 
