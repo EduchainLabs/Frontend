3b2093c184038a3f9cc17f72dfd893b57d64ddf5
AdityaAnandCodes <adityaanandatwork276@gmail.com>
2025-04-07 15:11:44 +0530

    feat: enhance Navbar with mobile menu functionality and improve LoginButton layout

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -14,5 +14,16 @@ export default function LoginButton() {
     }
   };
 
-  return <button className = "w-full px-5 py-2 bg-violet-700 hover:bg-violet-600 rounded-md flex gap-2 justify-center items-center text-white transition-all transform hover:scale-105 hover:shadow-lg hover:shadow-violet-500/30" onClick={handleLogin}><Logo /> <div>Connect <span className="font-semibold">OCID</span></div></button>;
+  return (
+    <button
+      className="w-auto px-3 sm:px-5 py-2 bg-violet-700 hover:bg-violet-600 rounded-md flex gap-1 sm:gap-2 justify-center items-center text-white transition-all transform hover:scale-105 hover:shadow-lg hover:shadow-violet-500/30"
+      onClick={handleLogin}
+    >
+      <Logo />
+      <div className="text-sm sm:text-base flex gap-1 justify-center items-center">
+        <span className="hidden sm:block">Connect </span>
+        <span className="font-semibold">OCID</span>
+      </div>
+    </button>
+  );
 }
